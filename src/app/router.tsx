6e4f22f1ada51aa221c1bdbc7601de81550be2a1
drews16 <109108583+drews16@users.noreply.github.com>
2025-04-01 20:10:08 +0400

    fix

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
-import { Home } from "../pages/Home/ui/Home";
+import { Home } from "@/pages/Home/ui/Home";
 
 export const router = createBrowserRouter([
     {
