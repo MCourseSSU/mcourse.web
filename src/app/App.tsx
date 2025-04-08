1e4f367552b022accaccfdbbb0089dcdc8f54835
Maria <mariaklyeva@gmail.com>
2025-04-08 21:03:44 +0400

    удаление NM, sheet

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,10 @@
 import { BrowserRouter } from 'react-router-dom';
-import NavigationMenu from '@/widgets/NavigationMenu/ui/NavigationMenu';
-import '../index.css';
 import './global.css'
 
 const App = () => {
   return (
     <BrowserRouter>
-      <NavigationMenu />
+
     </BrowserRouter>
   );
 };
